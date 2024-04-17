16e56eb90cdb89d8105735cf07790ff78e126e9f
Will Rocha-Thomas <withomas214@gmail.com>
2024-04-17 22:14:51 +0100

    Updates for Phoenix 1.7
    Changes taken from PR opened on the original repository https://github.com/mgwidmann/scrivener_html/pull/111/files. Sadly that PR has not been merged and there's still no sign of that original repository receiving updates.

--- test/scrivener/html/seo_test.exs
+++ test/scrivener/html/seo_test.exs
@@ -6,34 +6,35 @@ defmodule Scrivener.HTML.SEOTest do
 
   describe "#rel" do
     test "on the first page - page 2" do
-      assert rel(%Page{total_pages: 10, page_number: 1}, 2) == "next"
+      assert rel(%Scrivener.Page{total_pages: 10, page_number: 1}, 2) == "next"
     end
 
     test "on the first page - page 5" do
-      assert rel(%Page{total_pages: 10, page_number: 1}, 5) == "canonical"
+      assert rel(%Scrivener.Page{total_pages: 10, page_number: 1}, 5) == "canonical"
     end
 
     test "on the third page - page 4" do
-      assert rel(%Page{total_pages: 10, page_number: 3}, 4) == "next"
+      assert rel(%Scrivener.Page{total_pages: 10, page_number: 3}, 4) == "next"
     end
 
     test "on the third page - page 2" do
-      assert rel(%Page{total_pages: 10, page_number: 3}, 2) == "prev"
+      assert rel(%Scrivener.Page{total_pages: 10, page_number: 3}, 2) == "prev"
     end
 
     test "on the last page - page 9" do
-      assert rel(%Page{total_pages: 10, page_number: 10}, 9) == "prev"
+      assert rel(%Scrivener.Page{total_pages: 10, page_number: 10}, 9) == "prev"
     end
   end
 
   describe "#header_links" do
     test "on the first page" do
-      assert header_links(%Page{total_pages: 10, page_number: 1}) |> Phoenix.HTML.safe_to_string() ==
+      assert header_links(%Scrivener.Page{total_pages: 10, page_number: 1})
+             |> Phoenix.HTML.safe_to_string() ==
                {:safe,
                 [
                   60,
                   "link",
-                  [[32, "href", 61, 34, "?page=2", 34], [32, "rel", 61, 34, "next", 34]],
+                  [32, "href", 61, 34, "?page=2", 34, 32, "rel", 61, 34, "next", 34],
                   62,
                   [],
                   60,
@@ -45,13 +46,13 @@ defmodule Scrivener.HTML.SEOTest do
     end
 
     test "on the last page" do
-      assert header_links(%Page{total_pages: 10, page_number: 10})
+      assert header_links(%Scrivener.Page{total_pages: 10, page_number: 10})
              |> Phoenix.HTML.safe_to_string() ==
                {:safe,
                 [
                   60,
                   "link",
-                  [[32, "href", 61, 34, "?page=9", 34], [32, "rel", 61, 34, "prev", 34]],
+                  [32, "href", 61, 34, "?page=9", 34, 32, "rel", 61, 34, "prev", 34],
                   62,
                   [],
                   60,
@@ -63,33 +64,36 @@ defmodule Scrivener.HTML.SEOTest do
     end
 
     test "on a middle page" do
-      assert header_links(%Page{total_pages: 10, page_number: 5}) |> Phoenix.HTML.safe_to_string() ==
-               {:safe,
-                [
-                  [
-                    60,
-                    "link",
-                    [[32, "href", 61, 34, "?page=4", 34], [32, "rel", 61, 34, "prev", 34]],
-                    62,
-                    [],
-                    60,
-                    47,
-                    "link",
-                    62
-                  ],
-                  "\n",
-                  [
-                    60,
-                    "link",
-                    [[32, "href", 61, 34, "?page=6", 34], [32, "rel", 61, 34, "next", 34]],
-                    62,
-                    [],
-                    60,
-                    47,
-                    "link",
-                    62
-                  ]
-                ]}
+      assert header_links(%Scrivener.Page{total_pages: 10, page_number: 5})
+             |> Phoenix.HTML.safe_to_string() ==
+               {
+                 :safe,
+                 [
+                   [
+                     60,
+                     "link",
+                     [32, "href", 61, 34, "?page=4", 34, 32, "rel", 61, 34, "prev", 34],
+                     62,
+                     [],
+                     60,
+                     47,
+                     "link",
+                     62
+                   ],
+                   "\n",
+                   [
+                     60,
+                     "link",
+                     [32, "href", 61, 34, "?page=6", 34, 32, "rel", 61, 34, "next", 34],
+                     62,
+                     [],
+                     60,
+                     47,
+                     "link",
+                     62
+                   ]
+                 ]
+               }
                |> Phoenix.HTML.safe_to_string()
     end
   end
